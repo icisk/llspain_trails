29d23a645c385cd02fa296b4bdfeac22dba19898
lraeuschel <l.raeuschel@52north.org>
2024-11-07 12:30:53 +0100

    Add the icisk logo for the home tab

--- src/apps/andalusia-forecast/pages/Home.tsx
+++ src/apps/andalusia-forecast/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box } from "@open-pioneer/chakra-integration"
 import {InfoBoxComponent} from "info-box";
 import {useIntl} from "open-pioneer:react-hooks";
 
@@ -6,10 +7,13 @@ import {useIntl} from "open-pioneer:react-hooks";
 const Home = () => {
     const intl = useIntl();
     return (
-        <InfoBoxComponent
-            header={intl.formatMessage({id: "heading_home"})}
-            description={intl.formatMessage({id: "description_home"})}
-        ></InfoBoxComponent>
+        <Box>
+            <img src="/images/logo.png" alt = "I-CISK logo" style={{ width: "40%", height: "auto" }}/>
+            <InfoBoxComponent
+                header={intl.formatMessage({id: "heading_home"})}
+                description={intl.formatMessage({id: "description_home"})}
+            ></InfoBoxComponent>
+        </Box>
         
     )
 };
