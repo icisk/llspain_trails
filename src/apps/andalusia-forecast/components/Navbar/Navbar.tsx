3b3bc22add56468eb015fb39220f66f9b9d6bd4a
lraeuschel <l.raeuschel@52north.org>
2024-11-07 09:49:55 +0100

    Fixed the home tab and the navbar

--- src/apps/andalusia-forecast/components/Navbar/Navbar.tsx
+++ src/apps/andalusia-forecast/components/Navbar/Navbar.tsx
@@ -1,31 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import { Box, Flex, Text, Link, Stack } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <p>Andalusia Living Lab</p>
-                </li>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/forecast">Forecast</Link>
-                </li>
-                <li>
-                    <Link to="/historicclimatedata1">Historic Climate Data 1</Link>
-                </li>
-                <li>
-                    <Link to="/historicclimatedata2">Historic Climate Data 2</Link>
-                </li>
-                <li>
-                    <Link to="/biologicaleffectivedegreedays">Biological Effective Degree Days</Link>
-                </li>
-            </ul>
-        </nav>
+        <Box bg="gray.100" px={4} boxShadow="sm">
+            <Flex h={16} alignItems="center">
+                {/* Titel */}
+                <Text fontSize="lg" fontWeight="bold" color="gray.800" mr={8}>
+                    Andalusia Living Lab
+                </Text>
+
+                {/* Links */}
+                <Stack direction="row" spacing={6}>
+                    <Link as={RouterLink} to="/" color="gray.600" fontSize="sm" _hover={{ color: "gray.800" }}>
+                        Home
+                    </Link>
+                    <Link as={RouterLink} to="/forecast" color="gray.600" fontSize="sm" _hover={{ color: "gray.800" }}>
+                        Forecast
+                    </Link>
+                    <Link as={RouterLink} to="/historicclimatedata1" color="gray.600" fontSize="sm" _hover={{ color: "gray.800" }}>
+                        Historic Climate Data 1
+                    </Link>
+                    <Link as={RouterLink} to="/historicclimatedata2" color="gray.600" fontSize="sm" _hover={{ color: "gray.800" }}>
+                        Historic Climate Data 2
+                    </Link>
+                    <Link as={RouterLink} to="/biologicaleffectivedegreedays" color="gray.600" fontSize="sm" _hover={{ color: "gray.800" }}>
+                        Biological Effective Degree Days
+                    </Link>
+                </Stack>
+            </Flex>
+        </Box>
     );
 };
 
--- src/apps/andalusia-forecast/pages/Home.tsx
+++ src/apps/andalusia-forecast/pages/Home.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Box, Heading, Text, Image, VStack } from '@chakra-ui/react';
+import logo from '../images/logo.png'
 
-const Home = () => {
+const Home: React.FC = () => {
     return (
-        <>
-            <h3>Welcome!</h3>
-            <p>This webpage exhibits some first examples for the visualization of cimate and meteorological data for Living Lab Andalusia in Spain in the I-CISK project.</p>
-            <p>Navigate through the top bar to explore the different visualizations.</p>
-        </>
-        
-    )
+        <Box bg="gray.50" minHeight="100vh" py={10} px={8}>
+            {/* Logo */}
+            <Image src={logo} alt="Andalusia Living Lab Logo" mb={6}/>
+
+            {/* Inhalt */}
+            <VStack align="start" spacing={4}>
+                <Heading as="h3" size="lg" color="teal.600">
+                    Welcome!
+                </Heading>
+                <Text fontSize="md" color="gray.700">
+                    This webpage exhibits some first examples for the visualization of climate and meteorological data for Living Lab Andalusia in Spain in the I-CISK project.
+                </Text>
+                <Text fontSize="md" color="gray.600">
+                    Navigate through the top bar to explore the different visualizations.
+                </Text>
+            </VStack>
+        </Box>
+    );
 };
 
 export default Home;
