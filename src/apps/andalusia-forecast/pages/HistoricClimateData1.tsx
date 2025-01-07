c6ed9473ce813a5fd3ca81c367382941b8e10490
JohannesSchnell <j.schnell@52north.org>
2025-01-07 15:55:14 +0100

    not working: dataprep for plot

--- src/apps/andalusia-forecast/pages/HistoricClimateData1.tsx
+++ src/apps/andalusia-forecast/pages/HistoricClimateData1.tsx
@@ -18,12 +18,25 @@ import { MAP_ID2 } from '../services/HistoricClimateMapProvider2';
 import {DynamicPrecipitationLegend} from "../components/Legends/DynamicLegend";
 import { LayerSwipe } from '../components/LayerSwipe/LayerSwipe';
 import { isFormElement } from 'react-router-dom/dist/dom';
-import { map } from 'highcharts';
+import Highcharts, { map } from 'highcharts';
 import {HistoricPickerLeft} from "../components/VariablePicker/HistoricPickerLeft";
 import {HistoricPickerRight} from "../components/VariablePicker/HistoricPickerRight";
 import {useService} from "open-pioneer:react-hooks";
 import {Selection} from "../components/VariablePicker/HistoricPickerLeft";
 import {Knecht} from "../components/Legends/Knecht";
+import MapBrowserEvent from "ol/MapBrowserEvent";
+import {transform} from "ol/proj";
+import {createMarker, markerStyle} from "../components/utils/marker";
+import {useFetchData} from "../hooks/useFetchData";
+import { Vector as VectorLayer } from "ol/layer";
+import { Vector as VectorSource } from "ol/source";
+import * as domain from "node:domain";
+import HighchartsReact from "highcharts-react-official";
+
+
+// Marker layer for displaying clicks
+const markerSource = new VectorSource();
+const markerLayer = new VectorLayer({ source: markerSource, zIndex: 100 });
 
 const HistoricClimateData1 = () => {
     const intl = useIntl();
@@ -33,10 +46,96 @@ const HistoricClimateData1 = () => {
     const [leftLayers, setLeftLayers]= useState<Layer[]>();
     const [rightLayers, setRightLayers]= useState<Layer[]>();
     const [sliderValue, setSliderValue] = useState<number>(50);
-
-
     
+    const [clickedCoordinates, setClickedCoordinates] = useState<number[] | null>(null);
+
+    const [precipData, setPrecipData] = useState(null);
+    const [tempData, setTempData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    
+    const [precipTimeSeries, setPrecipTimeSeries] = useState<String>(null)
+    const [tempTimeSeries, setTempTimeSeries] = useState<String>(null)
+    const [longestTimeSeries, setLongestTimeSeries] = useState<String>(null)
+
+    const [chartOptions, setChartOptions] = useState({
+        chart: { type: 'column' },
+        title: { text: intl.formatMessage({ id: "global.plot.header_precip" }) },
+        xAxis: { categories: [] },
+        yAxis: { title: { text: "Precipitation (mm)" }, min: 0 },
+        series: [],
+        plotOptions: {
+            series: {
+                zones: []
+            }
+        }
+    });
+
     const mapModel = useMapModel(MAP_ID);
+    
+    useEffect(() => {
+        if (!clickedCoordinates) return;
+
+        const fetchPrecipData = async (x :any, y :any) => {
+            const url = `https://i-cisk.dev.52north.org/data/collections/creaf_historic_precip/position?coords=POINT(${x}%20${y})`;
+            try {
+                setLoading(true);
+                const response = await fetch(url);
+                if (!response.ok) throw new Error("Network response was not ok");
+                const jsonData = await response.json();
+                setPrecipData(jsonData);
+            } catch (err :any) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+        
+        const fetchTempData = async (x :any, y :any) => {
+            const url = `https://i-cisk.dev.52north.org/data/collections/creaf_historic_temperature/position?coords=POINT(${x}%20${y})`;
+            try {
+                setLoading(true);
+                const response = await fetch(url);
+                if (!response.ok) throw new Error("Network response was not ok");
+                const jsonData = await response.json();
+                setPrecipData(jsonData);
+            } catch (err :any) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };        
+
+        function createTimeSeries(start, stop) {
+            const startDate = new Date(start);
+            const stopDate = new Date(stop);
+            const timeSeries = [];
+
+            while (startDate <= stopDate) {
+                timeSeries.push(new Date(startDate).toISOString());
+                startDate.setMonth(startDate.getMonth() + 1); // Increment by 1 month
+            }
+            return timeSeries;
+        }
+
+
+        const [x, y] = clickedCoordinates
+        fetchPrecipData(x, y);
+        fetchTempData(x, y);
+        const precipStart = precipData?.domain?.axes?.time?.start
+        const precipStop = precipData?.domain?.axes?.time?.stop
+        const tempStart = tempData?.domain?.axes?.time?.start
+        const tempStop = tempData?.domain?.axes?.time?.stop
+        
+        setPrecipTimeSeries(createTimeSeries(precipStart, precipStop));
+        setTempTimeSeries(createTimeSeries(tempStart, tempStop))
+        if (precipTimeSeries != null && tempTimeSeries != null) {
+            setLongestTimeSeries(precipTimeSeries.length > tempTimeSeries.length ? precipTimeSeries : tempTimeSeries);
+        }
+        
+        console.log(longestTimeSeries);
+    }, [clickedCoordinates]);
+
 
     useEffect(() => {
         if(mapModel.map){
@@ -47,6 +146,70 @@ const HistoricClimateData1 = () => {
             setRightLayers([rightLayer1.olLayer as Layer])
         }
     }, [mapModel])
+
+    
+
+
+
+    useEffect(() => {
+        setChartOptions({
+            chart: {
+                type: "column",
+                zoomType: "x"
+            },
+            title: { text: intl.formatMessage({ id: "global.plot.header_precip" }) },
+            xAxis: { longestTimeSeries, title: {text: "Date"} },
+            yAxis:  [
+                {title: { text: "Precipitation (mm)" }, min: 0, max: 400, opposite: false},
+                {title: {text: "Temperatura (ºC)" }, min: -20, max: 50, opposite: true}
+            ],
+            series: [
+                {
+                    name: "Precipitation",
+                    data: precipData ? precipData?.ranges?.historic_precip?.values : null,
+                    type: "column",
+                    color: "blue",
+                    yAxis: 0
+                },
+                {
+                    name: "Temperatura",
+                    data: tempData ? tempData?.ranges?.historic_precip?.values : null,
+                    type: "spline",
+                    color: "orange",
+                    yAxis: 1
+                }
+            ]
+        });
+    }, [precipData, tempData]);
+    
+    
+    //click on map
+    useEffect(() => {
+        if (mapModel?.map?.olMap) {
+            const olMap = mapModel.map.olMap;
+            // add marker layer
+            olMap.addLayer(markerLayer);
+            // attach click event handler 
+            olMap.on("click", handleMapClick);
+            // Cleanup function 
+            return () => {
+                olMap.removeLayer(markerLayer);
+                olMap.un("click", handleMapClick);
+            };
+        }
+    }, [mapModel]);
+
+    const handleMapClick = (event: MapBrowserEvent<MouseEvent>) => {
+        const coordinatesEPSG3857 = event.coordinate;
+        const coordinatesEPSG25830 = transform(coordinatesEPSG3857, "EPSG:3857", "EPSG:25830");
+        //console.log("Clicked Coordinates (EPSG:25830):", coordinatesEPSG25830);
+        setClickedCoordinates(coordinatesEPSG25830);
+
+        //Clear previous markers and add a new one
+        markerSource.clear();
+        const marker = createMarker(coordinatesEPSG3857, markerStyle)
+        markerSource.addFeature(marker);
+    };
 
 
 
@@ -91,22 +254,26 @@ const HistoricClimateData1 = () => {
             <HistoricPickerLeft onChange={onLeftPickerChange}/>
             <HistoricPickerRight onChange={onRightPickerChange}/>
         </HStack>
-            <Container flex={2} minWidth={"container.xl"}>
-                <Box width="100%" height="600px" position="relative">
-                    <MainMap MAP_ID={MAP_ID}/>
-                    <DynamicPrecipitationLegend />
-                    <Knecht />
-                </Box>
+        <Container flex={2} minWidth={"container.xl"}>
+            <Box width="100%" height="600px" position="relative">
+                <MainMap MAP_ID={MAP_ID}/>
+                <DynamicPrecipitationLegend/>
+                <Knecht/>
+            </Box>
 
-                {(leftLayers && rightLayers && mapModel.map) &&
-                    <LayerSwipe map={mapModel.map} 
-                                sliderValue={sliderValue} 
-                                onSliderValueChanged={(newValue) => {setSliderValue(newValue)}} 
-                                leftLayers={leftLayers} 
-                                rightLayers={rightLayers} />
-                }
-            </Container>     
-   
+            {(leftLayers && rightLayers && mapModel.map) &&
+                <LayerSwipe map={mapModel.map}
+                            sliderValue={sliderValue}
+                            onSliderValueChanged={(newValue) => {
+                                setSliderValue(newValue)
+                            }}
+                            leftLayers={leftLayers}
+                            rightLayers={rightLayers}/>
+            }
+        </Container>
+        <div>
+            <HighchartsReact highcharts={Highcharts} options={chartOptions}/>
+        </div>
     </Box>
 </Box>
 
