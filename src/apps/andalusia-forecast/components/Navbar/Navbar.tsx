769621b36547418714920afc5e418454eb64c949
lraeuschel <l.raeuschel@52north.org>
2025-03-12 16:09:21 +0100

    fix the navbar for smaller window sizes

--- src/apps/andalusia-forecast/components/Navbar/Navbar.tsx
+++ src/apps/andalusia-forecast/components/Navbar/Navbar.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import './Navbar.css';
-import {Box, HStack} from "@chakra-ui/react";
-import {useIntl} from "open-pioneer:react-hooks";
+import React from "react";
+import "./Navbar.css";
+import { Box, HStack } from "@chakra-ui/react";
+import { useIntl } from "open-pioneer:react-hooks";
 import NavButton from "./NavButton";
 
 const Navbar: React.FC = () => {
     const intl = useIntl();
+    
     return (
         <Box style={{ padding: 20 }}>
-            <HStack spacing={4} justify="space-evenly" width="100%">
+            <HStack
+                spacing={2}
+                justify="center"
+                flexWrap="wrap"
+                width="100%"
+            >
                 <NavButton to="/" id="home.nav" />
                 <NavButton to="/forecast" id="forecast.nav" />
                 <NavButton to="/historicclimatedata1" id="historic_compare.nav" />
                 <NavButton to="/historicclimatestations" id="historic_stations.nav" />
-                {/*/<NavButton to="/historicclimatedata2" id="historic_station.nav" />*/}
                 <NavButton to="/projections" id="projections.nav" />
                 <NavButton to="/phenology" id="phenology.nav" />
                 <NavButton to="/hydrologicalservice" id="hydro_service.nav" />
